e6f6f18ad04c4831ac9454a886e2268b7ee88cad
lini07003 <lini07003@E-PW05C37N>
2024-09-11 12:25:24 +0200

    Add project files.

---
+++ Upg1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Upg1_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Blå bakgrundsfärg//
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            //Vit text//
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(":(");
+            Console.WriteLine("Det ser ut som din dator har fått ett problem och behöver startas om");
+        }
+    }
+}
